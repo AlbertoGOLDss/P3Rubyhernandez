bc31fbfe0680d73dfb99e20260cc33aa0788c250
Alberto Hernandez <740821@student.dusd.net>
2023-11-08 11:40:49 -0800

    rubys adventure update
    i added the rigid body on ruby and the boxes, and made some changed on the map.

--- RubyadventureP3Hernandez/Assets/Scripts/RubyController.cs
+++ RubyadventureP3Hernandez/Assets/Scripts/RubyController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-      
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
@@ -16,11 +22,14 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+    }
 
-        Vector2 position = transform.position;
-        position.x = position.x = + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y = + 3.0f * vertical * Time.deltaTime;
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime; ;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
