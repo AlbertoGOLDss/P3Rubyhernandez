30664138c5b129ec92e041c4810462db1f3334b4
Alberto Hernandez <740821@student.dusd.net>
2023-11-15 11:41:17 -0800

    ruby adventure update
    added the strawberry collectable and added the damage collectable with the box colliders in them.

--- RubyadventureP3Hernandez/Assets/Scripts/RubyController.cs
+++ RubyadventureP3Hernandez/Assets/Scripts/RubyController.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        currentHealth = maxHealth;
+        
+
     }
 
     // Update is called once per frame
@@ -23,13 +30,19 @@ public class RubyController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
     }
-
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime; ;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime; ;
 
         rigidbody2d.MovePosition(position);
     }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
+
